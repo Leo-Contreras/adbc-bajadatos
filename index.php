9e2728af7126a426e2cd219818e8262e61ca802f
TomasCarreon <60579917+TomasCarreon@users.noreply.github.com>
2023-06-27 13:33:28 -0700

    cambio
    cambios

--- index.php
+++ index.php
@@ -8,15 +8,112 @@
 	<link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css">
 </head>
 <body>
-	<?php
-		require("header.php");
-	?>
+	<div class="header-background">
+	    <header class="nocontent">
+	        <div id="header-mid">
+	            <div class="ic-container-fluid">
+	                <div id="logo" title="Clic para regresar a la página de inicio">
+	                    <p class="defaultLogo">
+	                        <a href="http://portalqa.bajacalifornia.gob.mx/bajadatos/"><img style="max-width:345px; outline:none!important" class="" src="assets/img/Agencia_Digital.png" alt="Logo BC"></a>
+	                    </p>
+	                </div>
+	                <div>
+	                    
+	                </div>
+	            </div>
+	        </div>
+	        <!--header-mid-->
+	        <div id="header-bottom">
+	            <div class="ic-container-fluid">
+	                <nav id="mainNav" aria-label="Navegación del sitio">
+	                    <ul id="nav" class="sf-js-enabled" style="color: white;">
+	                        <li id="navTab01" class="active">
+	                            <a title="Inicio" href="#" class="mainNavItem">Inicio</a>
+	                        </li>
+	                        <li id="navTab02">
+	                            <a title="Estadisticas" href="#" class="mainNavItem">Estadísticas</a>
+	                        </li>
+	                        <li id="navTab03">
+	                            <a title="Gobierno" href="#" class="mainNavItem">Gobierno Abierto</a>
+	                        </li>
+	                        <li id="navTab04">
+	                            <a title="Actividades" href="#" class="mainNavItem">Actividades</a>
+	                        </li>
+	                        <li id="navTab05">
+	                            <a title="Solicitud" href="#" class="mainNavItem">Solicitudes de información recurrentes</a>
+	                        </li>
+	                        <li id="navTab06">
+	                            <a title="Buzon" href="#" class="mainNavItem">Buzón Ciudadano</a>
+	                        </li>
+	                    </ul>
+	                </nav>
+	            </div>
+	        </div>
+	        <!--header-bottom-->
+	    </header>
+	</div>
 	<div style="padding: 50px;">
 		
 	</div>
-	<?php
-		require("footer.php");
-	?>
+	<footer class="page-footer font-small text-center" style="margin-top: 70px">
+	    <div class="footer-line-yellow"></div>
+	    <div class="row" style="background-color:#762d41">
+	        <div class="col-md-3 px-5" style="border-right:2px solid #bd945c;  margin-top:3rem!important; margin-bottom:3rem!important">
+	            <div class="visible-lg visible-md ">
+	                <img style="width:240px;" alt="Logo de Baja California" class="mt-3" src="assets/img/bc_blanco.png">
+	            </div>
+	            <div class="visible-sm visible-xs text-center">
+	                <img alt="Logo de Baja California" class="mt-3" style="width:30%" src="assets/img/bc_blanco.png">
+	            </div>
+	        </div>
+	        <div class="col-md-9 px-5">
+	            <div class="row footer-enlaces" style="margin-top:25px!important;">
+	                <div class="col-md-5">
+	                    <ul>
+	                        <li><a href="http://portalqa.bajacalifornia.gob.mx/bajadatos/Actividades"> Actividades</a></li>
+	                        <li><a href="http://portalqa.bajacalifornia.gob.mx/bajadatos/#">Estadísticas</a></li>
+	                        <li><a href="http://portalqa.bajacalifornia.gob.mx/bajadatos/#">Buzón ciudadano</a></li>
+	                        <li><a href="http://portalqa.bajacalifornia.gob.mx/bajadatos/SolicitudInformacion">Solicitud de información</a></li>
+	                        <li><a href="http://portalqa.bajacalifornia.gob.mx/bajadatos/SitiosInteres">Sitios de interés</a></li>
+	                    </ul>                           
+	                </div>
+	                <div class="col-md-4">
+	                    <ul>
+	                        <li><a href="http://portalqa.bajacalifornia.gob.mx/bajadatos/#"> Agencia Digital</a></li>
+	                        <li><a href="http://portalqa.bajacalifornia.gob.mx/bajadatos/#"> Coordinación de Gabinete</a></li>
+	                        <li><a href="http://portalqa.bajacalifornia.gob.mx/bajadatos/#"> Secretaría de Hacienda</a></li>
+	                        <li><a href="http://portalqa.bajacalifornia.gob.mx/bajadatos/#"> Secretaría de Honestidad</a></li>
+	                    </ul>
+	                </div>
+	                <div class="col-md-3">
+	                    <div class="row text-right">
+	                        <div class="col-md-6">
+	                            <div class="txtb text-center">Síguenos:</div>
+	                            <div class="row">
+	                                <div class="col-md-4">
+	                                    <a class="nav-link colorIcons" title="Síguenos en Facebook" href="https://www.facebook.com/BC.Gobierno" target="_blank"><i class="fab fa-facebook-f" aria-hidden="true"></i></a>
+	                                </div>
+	                                <div class="col-md-4">
+	                                    <a class="nav-link colorIcons" title="Síguenos en Twitter" href="https://twitter.com/bcgobierno" target="_blank"><i class="fab fa-twitter" aria-hidden="true"></i></a>
+	                                </div>
+	                                <div class="col-md-4">
+	                                    <a class="nav-link colorIcons" title="Síguenos en Instagram" href="https://www.instagram.com/BCGobierno/" target="_blank"><i class="fab fa-instagram" aria-hidden="true"></i></a>
+	                                </div>
+	                            </div>
+	                        </div>
+	                        <div class="col-md-6"></div>
+	                    </div>
+	                </div>
+	            </div>   
+	        </div>
+	        <div class="row">
+	            <div class="col-md-12">
+	                <p style="color:#ffffff;  font-size:13px; margin-top:10px;">© Gobierno del Estado de Baja California 2022 - Bajadatos </p>
+	            </div>
+	        </div>
+	    </div>
+	    <div id="pie-guinda"></div>
+	</footer>
 	<script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
 </body>
 </html>
